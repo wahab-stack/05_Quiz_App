7ba482bce9efe83689f2f6a1092634f6c59d975c
Wahab1437 <wahab.suit997@gmail.com>
2025-05-25 16:11:15 +0500

    Adding some javaScript

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //Grabing Ids from HTML
+  const startBtn = document.getElementById("start-btn");
+  const nextBtn = document.getElementById("next-btn");
+  const restartBtn = document.getElementById("restart-btn");
+  const questionContainer = document.getElementById("question-container");
+  const resultContainer = document.getElementById("result-container");
+  const questionText = document.getElementById("question-text");
+  const choicesList = document.getElementById("choices-list");
+  const scoreDisplay = document.getElementById("score");
+
+  const questions = [
+    {
+      question: "What is the capital of France",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      choices: ["Mars", "Venus", "Jupiter", "Saturn"],
+      answer: "Mars",
+    },
+    {
+      question: "Which river is the longest in the world?",
+      choices: ["Amazon", "Nile", "Yangtze", "Mississippi"],
+      answer: "Nile",
+    },
+  ];
+
+  let currentQuestionIndex = 0;
+  let score = 0;
+
+  startBtn.addEventListener("click", startQuiz);
+
+  function startQuiz() {
+    startBtn.classList.add("hidden");
+    resultContainer.classList.add("hidden");
+    questionContainer.classList.remove("hidden");
+
+    showQuestion();
+  }
+
+  function showQuestion() {
+    nextBtn.classList.add("hidden");
+    questionText.textContent = questions[currentQuestionIndex].question;
+    choicesList.innerHTML = ""; //Clear Previous Choices
+
+    questions[currentQuestionIndex].choices.forEach((choice) => {
+      const li = document.createElement("li");
+      li.textContent = choice;
+      li.addEventListener("click", () => selectAnswer(choice));
+      choicesList.appendChild(li);
+    });
+  }
+
+  function selectAnswer(choice) {}
+});
